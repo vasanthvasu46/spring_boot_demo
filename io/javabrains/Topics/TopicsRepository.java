40eda218bc068a6f7a11fd9b36bc2862b219cad3
Vasanth M <100766595+vasanthvasu46@users.noreply.github.com>
2022-04-20 21:46:32 +0530

    JPA

---
+++ io/javabrains/Topics/TopicsRepository.java
@@ -0,0 +1,8 @@
+package io.javabrains.Topics;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicsRepository extends CrudRepository<Topics_class, Integer> {
+
+
+}
