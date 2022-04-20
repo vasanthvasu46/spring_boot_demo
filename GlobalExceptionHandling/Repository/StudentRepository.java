ebcb995b24eb5ffa1509d5ef3911834c3e02cfde
Vasanth M <100766595+vasanthvasu46@users.noreply.github.com>
2022-04-20 21:38:57 +0530

    Delete GlobalExceptionHandling directory

--- GlobalExceptionHandling/Repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package GlobalExceptionHandling.Repository;
-
-import GlobalExceptionHandling.Model.Students;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Students, Integer> {
-}
