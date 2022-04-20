f2a01d5b740cff4973647d5dcbe0a26c08c501d6
Vasanth M <100766595+vasanthvasu46@users.noreply.github.com>
2022-04-20 21:38:46 +0530

    Delete CutsomExceptionHandling directory

--- CutsomExceptionHandling/Repository/MoviesRepository.java
+++
@@ -1,8 +0,0 @@
-package CutsomExceptionHandling.Repository;
-
-import CutsomExceptionHandling.Model.Movies;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MoviesRepository extends CrudRepository<Movies, String> {
-
-}
